2b1ae2ee377e94dc8996b90fd14be5897daa09c0
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-06-10 20:56:44 +0200

    Hide menu when clicking on link

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,5 +1,7 @@
 const smoothScroll = require('smooth-scroll/dist/js/smooth-scroll');
 
+const $nav = document.querySelector('.a-mobile-menu');
+
 smoothScroll.init({
     selector: '[data-scroll]',
     selectorHeader: '[data-scroll-header]',
@@ -7,4 +9,10 @@ smoothScroll.init({
     easing: 'easeOutQuart',
     offset: 0,
     updateURL: false
+});
+
+[].slice.call(document.querySelectorAll('[data-scroll]')).forEach(function ($item) {
+    $item.addEventListener('click', function () {
+        $nav.classList.remove('a-mobile-menu--visible');
+    }, false);
 });
