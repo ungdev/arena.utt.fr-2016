a5bd6944e27839685360789cdc3234b2ee5f0be9
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-08-07 23:18:51 +0200

    Fix countdown

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -5,20 +5,14 @@ const pad2 = n => (n < 10 ? '0' : '') + n;
 const startDate = new Date('2016-11-09T16:00:00+00:00');
 const diff = countdown(startDate);
 
-const months = pad2(diff.months).split('');
-const days   = pad2(diff.days).split('');
-const hours  = pad2(diff.hours).split('');
+const months = pad2(diff.months);
+const days   = pad2(diff.days);
+const hours  = pad2(diff.hours);
 
-const $countdownMonthsOne = document.querySelector('.a-countdown__months--one');
-const $countdownMonthsTwo = document.querySelector('.a-countdown__months--two');
-const $countdownDaysOne = document.querySelector('.a-countdown__days--one');
-const $countdownDaysTwo = document.querySelector('.a-countdown__days--two');
-const $countdownHoursOne = document.querySelector('.a-countdown__hours--one');
-const $countdownHoursTwo = document.querySelector('.a-countdown__hours--two');
+const $countdownMonths = document.querySelector('.a-countdown__months');
+const $countdownDays   = document.querySelector('.a-countdown__days');
+const $countdownHours  = document.querySelector('.a-countdown__hours');
 
-$countdownMonthsOne.innerHTML = months[0];
-$countdownMonthsTwo.innerHTML = months[1];
-$countdownDaysOne.innerHTML = days[0];
-$countdownDaysTwo.innerHTML = days[1];
-$countdownHoursOne.innerHTML = hours[0];
-$countdownHoursTwo.innerHTML = hours[1];
+$countdownMonths.innerHTML = months;
+$countdownDays.innerHTML = days;
+$countdownHours.innerHTML = hours;
