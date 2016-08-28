a2eef8246f634c3626d8dcc563d7fff30b0355c1
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-08-28 12:11:38 +0200

    Remove animation on tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -4,26 +4,17 @@ const activatePanel = function ($sourceTab) {
     const $tabContainer   = $sourceTab.parentElement;
     const $panelContainer = $tabContainer.nextElementSibling;
 
-
-    const $activeTab = $tabContainer.querySelector('.a-category__tabs__tab--active');
-    const index      = Array.from($tabContainer.children).indexOf($activeTab);
-
     const $activePanel = $panelContainer.querySelector('.a-category__panels__panel--active');
     $activePanel.classList.remove('a-category__panels__panel--active');
 
+    const $activeTab = $tabContainer.querySelector('.a-category__tabs__tab--active');
+    const index      = Array.from($tabContainer.children).indexOf($activeTab);
     const $newTab = $panelContainer.children[index];
-
-    setTimeout(() => {
-        $activePanel.style.display = 'none';
-
-        $newTab.style.display = 'block';
-        $newTab.clientWidth; // reflow
-        $newTab.classList.add('a-category__panels__panel--active');
-    }, 200);
+    $newTab.classList.add('a-category__panels__panel--active');
 };
 
 $tabs.forEach($tab => {
-    $tab.children[0].addEventListener('click', e => {
+    $tab.addEventListener('click', e => {
         e.preventDefault();
 
         const $activeTab = $tab.parentElement.querySelector('.a-category__tabs__tab--active');
