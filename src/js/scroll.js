377f0d73a513f6b4cd7f3e9b33b9f8889c59fcfe
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-09-21 15:03:40 +0200

    Fix scrollers

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -3,17 +3,19 @@ const smoothScroll = require('smooth-scroll/dist/js/smooth-scroll');
 const $nav       = document.querySelector('.a-mobile-menu');
 const $scrollers = Array.from(document.querySelectorAll('[data-scroll]'));
 
-smoothScroll.init({
-    selector: '[data-scroll]',
-    selectorHeader: '[data-scroll-header]',
-    speed: 750,
-    easing: 'easeOutQuart',
-    offset: 0,
-    updateURL: false
-});
+setTimeout(() => {
+    smoothScroll.init({
+        selector: '[data-scroll]',
+        selectorHeader: '[data-scroll-header]',
+        speed: 750,
+        easing: 'easeOutQuart',
+        offset: 0,
+        updateURL: false
+    });
 
-$scrollers.forEach(function ($item) {
-    $item.addEventListener('click', function () {
-        $nav.classList.remove('a-mobile-menu--visible');
-    }, false);
-});
+    $scrollers.forEach(function ($item) {
+        $item.addEventListener('click', function () {
+            $nav.classList.remove('a-mobile-menu--visible');
+        }, false);
+    });
+}, 50);
