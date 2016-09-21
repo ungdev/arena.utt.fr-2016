7ff8932ac3c707e2e1ef89b419aa00fa873e242d
Gabriel JUCHAULT <gabriel.juchault@gmail.com>
2016-09-21 15:36:12 +0200

    fix scroll offsets

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,21 +1,33 @@
 const smoothScroll = require('smooth-scroll/dist/js/smooth-scroll');
 
+const $menu      = document.querySelector('.a-menu');
 const $nav       = document.querySelector('.a-mobile-menu');
 const $scrollers = Array.from(document.querySelectorAll('[data-scroll]'));
 
-setTimeout(() => {
-    smoothScroll.init({
-        selector: '[data-scroll]',
-        selectorHeader: '[data-scroll-header]',
-        speed: 750,
-        easing: 'easeOutQuart',
-        offset: 0,
-        updateURL: false
-    });
 
-    $scrollers.forEach(function ($item) {
-        $item.addEventListener('click', function () {
-            $nav.classList.remove('a-mobile-menu--visible');
-        }, false);
-    });
+let isMenuFixed;
+let offset;
+
+setTimeout(() => {
+    isMenuFixed = $menu.classList.contains('a-menu--fixed-top');
+    offset = isMenuFixed ? 0 : -75;
 }, 50);
+
+$scrollers.forEach(function ($item) {
+    $item.addEventListener('click', function () {
+        const $target = $item.getAttribute('href');
+
+        if ($target === '#a-anchor--event') {
+            offset = 0;
+        }
+
+        smoothScroll.animateScroll($target, $item, {
+            speed: 750,
+            easing: 'easeOutQuart',
+            offset: offset,
+            updateURL: false
+        });
+
+        $nav.classList.remove('a-mobile-menu--visible');
+    }, false);
+});
